d0ac7865619bf7cdd987162121e74df5c956519f
romain.fontugne <romain.fontugne@gmail.com>
2018-01-07 22:06:06 +0900

    added parameter sensitivity analysis

---
+++ scripts/alphaSensitivity.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from datetime import timedelta
+import calendar
+import os
+import pytz 
+import numpy as np
+
+date = datetime(2017, 12, 15, tzinfo = pytz.utc)
+dateStr = "%s-%s-%sT%02d:%02d" % (date.year, date.month, date.day, date.hour, date.minute)
+alphaValues = np.arange(0,0.51, 0.1)
+alphaValues[-1] = 0.49
+
+outputDirectory = "alphaSensitivity/"
+cmd = 'python2 src/ashash.py -c route-views3 -o %s %s %s' % ( outputDirectory, dateStr, dateStr )
+print(cmd)
+os.system(cmd)
