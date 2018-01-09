011f236c6e96e802a1f275f9721492adb0f51449
romain.fontugne <romain.fontugne@gmail.com>
2018-01-09 22:30:59 +0900

    plot alpha sensitivity

--- scripts/alphaSensitivity.py
+++ scripts/alphaSensitivity.py
@@ -9,7 +9,7 @@ date = datetime(2017, 12, 15, tzinfo = pytz.utc)
 dateStr = "%s-%s-%sT%02d:%02d" % (date.year, date.month, date.day, date.hour, date.minute)
 alphaValues = np.arange(0,0.51, 0.1)
 alphaValues[-1] = 0.49
-outputDirectory = "alphaSensitivity/"
+outputDirectory = "results/alphaSensitivity/"
 
 for alpha in alphaValues:
     cmd = 'python2 src/ashash.py --alpha %s -c route-views3 -o %s %s %s' % (alpha, outputDirectory, dateStr, dateStr )
