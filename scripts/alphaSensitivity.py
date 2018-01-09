8d9892ed098399bf90cb2e2cbedf8116c6b1c209
romain.fontugne <romain.fontugne@gmail.com>
2018-01-09 14:51:26 +0000

    sync

--- scripts/alphaSensitivity.py
+++ scripts/alphaSensitivity.py
@@ -7,12 +7,12 @@ import numpy as np
 
 date = datetime(2017, 12, 15, tzinfo = pytz.utc)
 dateStr = "%s-%s-%sT%02d:%02d" % (date.year, date.month, date.day, date.hour, date.minute)
-alphaValues = np.arange(0,0.51, 0.1)
-alphaValues[-1] = 0.49
+alphaValues = np.arange(0.05,0.51, 0.1)
+# alphaValues[-1] = 0.49
 outputDirectory = "alphaSensitivity/"
 
 for alpha in alphaValues:
-    cmd = 'python2 src/ashash.py --alpha %s -c route-views3 -o %s %s %s' % (alpha, outputDirectory, dateStr, dateStr )
+    cmd = 'python2 src/ashash.py --alpha %s -o %s %s %s' % (alpha, outputDirectory, dateStr, dateStr )
     print(cmd)
     os.system(cmd)
 
