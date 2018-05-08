576ba5efc4893fd7ca340a13d12abb1078e17207
romain.fontugne <romain.fontugne@gmail.com>
2018-05-08 11:25:08 +0900

    fixed the onlyFullFeed option

--- src/ashash.py
+++ src/ashash.py
@@ -59,7 +59,7 @@ endtime = valid_date(config_parser.get("date","endtime",False,argsDict))
 collector = [x.strip() for x in config_parser.get("peers","collector",False,argsDict).split(",")  if x.strip() != ""]
 includedPeers = [x.strip() for x in config_parser.get("peers","include",False,argsDict).split(",")  if x.strip() != ""]
 excludedPeers = [x.strip() for x in config_parser.get("peers","exclude",False,argsDict).split(",")  if x.strip() != ""]
-onlyFullFeed = bool(config_parser.get("peers","onlyFullFeed",True,argsDict))
+onlyFullFeed = bool(int(config_parser.get("peers","onlyfullfeed")))
 af = int(config_parser.get("origins","af",False,argsDict))
 spatial = int(config_parser.get("origins","spatial",False,argsDict))
 includedOrigins = [x.strip() for x in config_parser.get("origins","include",False,argsDict).split(",") if x.strip() != ""]  
